316a6767efd4c24c0dd894d4ba8f7de67cce435d
djd378 <djd378@drexel.edu>
2019-07-05 03:03:27 -0400

    Added files.

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 Fri Jul 5 00:50:46 EDT 2019"
+#define LINUX_COMPILE_TIME "00:50:46"
+#define LINUX_COMPILE_BY "djd378"
+#define LINUX_COMPILE_HOST "tux2"
+#define LINUX_COMPILE_DOMAIN "cci.drexel.edu"
+#define LINUX_COMPILER "gcc version 7.4.0 (Ubuntu 7.4.0-1ubuntu1~18.04.1) "
